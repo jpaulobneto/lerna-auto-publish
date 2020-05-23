2ee5736e028ec8925204ab65c13789a9d7982055
João Paulo Barbosa <jpaulobneto@gmail.com>
2020-05-23 15:21:58 -0300

    chore: update calc tests

--- packages/apps/calc/__tests__/calc.test.js
+++ packages/apps/calc/__tests__/calc.test.js
@@ -11,5 +11,17 @@ describe("calc", () => {
     it("returns the sub of passed numbers", () => {
       expect(calc("sub", 0, 1, 2, 3, 4)).toEqual(-10);
     });
+
+    describe("when multi", () => {
+      it("returns the multi of passed numbers", () => {
+        expect(calc("multi", 1, 2, 3, 4, 5)).toEqual(120);
+      });
+    });
+
+    describe("when divi", () => {
+      it("returns the division of passed numbers", () => {
+        expect(calc("divi", 100, 2, 5, 2.5)).toEqual(4);
+      });
+    });
   });
 });
